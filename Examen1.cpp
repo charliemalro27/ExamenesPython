747b30f5cb73836ae25376da19d6d6e0fb80a51b
charliemalro27 <carlos.maldonado@itgranada.com>
2022-02-04 11:02:11 +0100

    Divisores (sin funcionar)

--- Examen1.cpp
+++ Examen1.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
+//Devolver divisores de un numero en una lista
+void divisoresnumero(int numero){
+	for(int i=0;i<numero;i++){
+		int divisores[i];
+		if(numero%i==0)
+		divisores[i]=numero;
+		cout<<divisores[i]<<endl;
+	}
+}
+//Imprimir lista
+void imprimirlista(string lista[],int tamano){
+	for(int i=0;i<tamano;i++){
+		cout<<lista[i]<<endl;
+	}
+}
+
 int main(){
+
 		cout<<"Examen Bucles 1"<<endl;
+
+		int numero;
+		divisoresnumero(21);
 
 
 
